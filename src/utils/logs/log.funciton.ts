e0b18bc28be4bcac94afc3e6862e0601c59a2351
Nikita <nikitatsarenko7@gmail.com>
2025-05-04 10:44:40 +0300

    Implement logs

---
+++ src/utils/logs/log.funciton.ts
@@ -0,0 +1,11 @@
+import {LogEntry} from "@/pages/AdminLogs/types.ts";
+import {logsTable} from "@/integrations/supabase/client.ts";
+import { v4 as uuid } from "uuid"
+
+export async function log(logData: Omit<Omit<LogEntry, 'timestamp'>, 'id'>){
+    const logEntry: LogEntry = Object.assign(logData, { id: uuid(), timestamp: new Date})
+
+    await logsTable().insert([logEntry])
+}
+
+
